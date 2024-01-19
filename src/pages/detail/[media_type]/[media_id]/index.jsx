e53dbf739c580808d573733f21b0bf112e7efd43
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2024-01-20 08:17:39 +0900

    日本語詳細がない場合に英語詳細を表示する

--- src/pages/detail/[media_type]/[media_id]/index.jsx
+++ src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,14 +1,88 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import axios from 'axios'
-import React from 'react'
+import Head from 'next/head'
+import React, { StrictMode } from 'react'
 
 const Detail = props => {
     const { detail } = props
 
     return (
-        <>
-            <h1>{detail.title}</h1>
-            <div>Detail</div>
-        </>
+        <StrictMode>
+            <AppLayout
+                header={
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                        Detail
+                    </h2>
+                }>
+                <Head>
+                    <title>Laravel - Detail</title>
+                </Head>
+                <Box
+                    sx={{
+                        height: { sx: 'auto', md: '70vh' },
+                        position: 'relative',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}>
+                    <Box
+                        sx={{
+                            backgroundImage: `url(https:image.tmdb.org/t/p/original${detail.backdrop_path})`,
+                            position: 'absolute',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+
+                            '&:before': {
+                                content: '""',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                backgroundColor: `rgba(0,0,0,0.5)`,
+                                backdropFilter: 'blur(10px)',
+                            },
+                        }}
+                    />
+                    <Container sx={{ zIndex: 1 }}>
+                        <Grid
+                            container
+                            alignItems={'center'}
+                            sx={{ color: 'white' }}>
+                            <Grid
+                                item
+                                md={4}
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                }}>
+                                <img
+                                    width={'70%ß'}
+                                    alt={detail.title}
+                                    src={`https:image.tmdb.org/t/p/original${detail.backdrop_path}`}
+                                />
+                            </Grid>
+                            <Grid item md={8}>
+                                <Typography variant="h4" paragraph>
+                                    {detail.title}
+                                </Typography>
+                                <Typography paragraph>
+                                    {detail.overview}
+                                </Typography>
+                                <Typography valiant="h6">
+                                    公開日：{detail.release_date}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </AppLayout>
+        </StrictMode>
     )
 }
 
@@ -18,13 +92,19 @@ export async function getServerSideProps(context) {
     // サーバーサイドなので、process.envから読み込んでもブラウザ側へ表示されることがない。このことを意識して利用する
 
     try {
-        const response = await axios.get(
-            `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja_JP`,
+        const JpResponse = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`,
         )
-        const fetchData = response.data
+        let combinedData = { ...JpResponse.data }
+        if (!JpResponse.data.overview) {
+            const EnResponse = await axios.get(
+                `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=en-EN`,
+            )
+            combinedData.overview = EnResponse.data.overview
+        }
         return {
             props: {
-                detail: fetchData,
+                detail: combinedData,
             },
         }
     } catch (error) {
