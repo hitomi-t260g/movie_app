5e259739b544f07268e920271f5de9cdbd1765e0
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2024-02-03 05:49:21 +0900

    共通ヘッダーの追加

--- src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
+++ src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
@@ -1,7 +1,21 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import Head from 'next/head'
 import React from 'react'
 
 const ReviewDetail = () => {
-    return <div>レビュー詳細ページです</div>
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    ReviewDetail
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - ReviewDetail</title>
+            </Head>
+            <div>レビュー詳細ページです</div>
+        </AppLayout>
+    )
 }
 
 export default ReviewDetail
