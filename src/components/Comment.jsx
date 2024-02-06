d39dc0ddb05d5bb8ef8f944d3e67a286752f2cbd
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2024-02-07 02:26:39 +0900

    コメント編集のUI作成

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,12 +4,23 @@ import {
     Card,
     CardContent,
     Grid,
+    TextareaAutosize,
     Typography,
 } from '@mui/material'
 import React from 'react'
 
 const Comment = props => {
-    const { userName, content, id, handleDelete } = props
+    const {
+        userName,
+        content,
+        id,
+        editMode,
+        editedComment,
+        handleDelete,
+        handleEdit,
+        handleConfirmEdit,
+        setEditedComment,
+    } = props
 
     return (
         <Card>
@@ -17,27 +28,56 @@ const Comment = props => {
                 <Typography variant="h6" component="div" gutterBottom>
                     {userName}
                 </Typography>
-
-                <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    gutterBottom
-                    paragraph>
-                    {content}
-                </Typography>
+                {editMode === id ? (
+                    // 編集時のコメント表示
+                    <TextareaAutosize
+                        value={editedComment}
+                        minRows={3}
+                        style={{
+                            width: '100%',
+                        }}
+                        onChange={e => setEditedComment(e.target.value)}
+                    />
+                ) : (
+                    //通常時のコメント表示
+                    <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                        gutterBottom
+                        paragraph>
+                        {content}
+                    </Typography>
+                )}
 
                 <Grid container justifyContent="flex-end">
                     <ButtonGroup sx={{ gap: 1 }}>
-                        <Button
-                            //  muiの何かがうまくいかず、marginをつけると右側の線が消えてしまうため直接指定する
-                            style={{
-                                borderRight:
-                                    '1px solid rgba(25, 118, 210, 0.5)',
-                                borderRadius: '4px',
-                            }}>
-                            編集
-                        </Button>
+                        {editMode === id ? (
+                            // 編集時のボタン表示
+                            <Button
+                                //  muiの何かがうまくいかず、marginをつけると右側の線が消えてしまうため直接指定する
+                                style={{
+                                    borderRight:
+                                        '1px solid rgba(25, 118, 210, 0.5)',
+                                    borderRadius: '4px',
+                                }}
+                                onClick={handleConfirmEdit}>
+                                編集完了
+                            </Button>
+                        ) : (
+                            // 通常時のボタン表示
+                            <Button
+                                //  muiの何かがうまくいかず、marginをつけると右側の線が消えてしまうため直接指定する
+                                style={{
+                                    borderRight:
+                                        '1px solid rgba(25, 118, 210, 0.5)',
+                                    borderRadius: '4px',
+                                }}
+                                onClick={handleEdit}>
+                                編集
+                            </Button>
+                        )}
+
                         <Button color="error" onClick={() => handleDelete(id)}>
                             削除
                         </Button>
--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,10 +1,13 @@
 import { Grid } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import Comment from './Comment'
 import laravelAxios from '@/lib/laravelAxios'
 
 const CommentList = props => {
     const { comments, setComments } = props
+    // 編集用state
+    const [commentEditMode, setCommentEditMode] = useState(null)
+    const [editedComment, setEditedComment] = useState('')
 
     const handleDeleteComment = async commentId => {
         if (window.confirm('コメントを削除してもよろしいですか？')) {
@@ -29,6 +32,40 @@ const CommentList = props => {
         }
     }
 
+    const handleEditComment = comment => {
+        setCommentEditMode(comment.id)
+        // 編集ボタン押下時のコメントを編集前にセーブするため、editedCommentに現在のコメントをセットする
+        setEditedComment(comment.content)
+    }
+
+    const handleConfirmEditComment = async commentId => {
+        setCommentEditMode(null)
+        try {
+            // サーバー側のコメントを編集する
+            const response = await laravelAxios.put(
+                `api/comment/${commentId}`,
+                {
+                    content: editedComment,
+                },
+            )
+            // クライアント側のコメントを更新する
+            if (response.status === 200) {
+                const updatedComments = comments.map(comment => {
+                    if (comment.id === commentId) {
+                        return {
+                            ...comment,
+                            content: editedComment,
+                        }
+                    }
+                    return comment
+                })
+                setComments(updatedComments)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <Grid container spacing={3}>
             {comments.map(comment => (
@@ -36,8 +73,15 @@ const CommentList = props => {
                     <Comment
                         userName={comment.user.name}
                         content={comment.content}
-                        handleDelete={handleDeleteComment}
                         id={comment.id}
+                        handleDelete={handleDeleteComment}
+                        handleEdit={() => handleEditComment(comment)}
+                        handleConfirmEdit={() =>
+                            handleConfirmEditComment(comment.id)
+                        }
+                        editMode={commentEditMode}
+                        editedComment={editedComment}
+                        setEditedComment={setEditedComment}
                     />
                 </Grid>
             ))}
