9cbbfbe6115a96d1af57ce106508e8fd31d06ea2
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2024-02-26 04:27:37 +0900

    swrによるfetchを追加

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,8 +1,17 @@
 import AppLayout from '@/components/Layouts/AppLayout'
+import laravelAxios from '@/lib/laravelAxios'
 import Head from 'next/head'
 import React from 'react'
+import useSWR from 'swr'
 
 const favorites = () => {
+    const fetcher = url => laravelAxios.get(url).then(res => res.data)
+    // SWRを利用し、同一ユーザーで特定動作をしていなければキャッシュ内容を返す
+    const { data: favoriteItems, error } = useSWR('api/favorites', fetcher)
+    console.log(favoriteItems)
+    if (error) {
+        return <div>エラーが発生しました</div>
+    }
     return (
         <AppLayout
             header={
@@ -13,9 +22,6 @@ const favorites = () => {
             <Head>
                 <title>Laravel - Favorites</title>
             </Head>
-
-
-        
         </AppLayout>
     )
 }
